42837b5048e1d6e8c3423cce47b6bc731298d88e
Valery Bugakov <skymk1@gmail.com>
2018-08-12 11:51:58 +0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: `redux-boost{production ? '.min' : ''}.js`,
+    filename: `redux-boost${production ? '.min' : ''}.js`,
     library: 'ReduxBoost',
     libraryTarget: 'umd',
   },
